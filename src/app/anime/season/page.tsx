6d3b51af4e9c6018810c4baffba88799da5eae0a
Tedi Suryadi <tedi.suryadi77@gmail.com>
2023-11-09 10:45:58 +0700

    loading routing

--- src/app/anime/season/page.tsx
+++ src/app/anime/season/page.tsx
@@ -1,24 +1,31 @@
-import { Layout, Card } from "@/components";
-import { seasonsNow, mostPopuler } from '@/api';
+"use client";
 
-const Season = async () => {
+import { Layout } from "@/components";
+import { useRouter } from 'next/navigation'
 
-	const animeSeasonsNow = await seasonsNow(6)
-	const animePopuler = await mostPopuler(6)
+const Page = () => {
+
+    const router = useRouter()
+
+    const backPage = (e: any) => {
+        e.preventDefault();
+        router.back()
+    }
 
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between'>
 			<Layout>
-				<div className='w-full rounded-box l:px-28 m:px-14 py-2 s:px-10'>
-					<div className='card shadow-xl'>
-						<div className='bg-base-100 rounded-t-lg'>
-							<div className='w-full'>
-								<h5 className='pt-2 pl-6 font-semibold text-xl'>{ `${animeSeasonsNow.data[0].season} ${animeSeasonsNow.data[0].year}` }</h5>
-							</div>
-						</div>
-						<div className='w-full bg-gray-500 rounded-b-lg'>
-							<Card anime={animeSeasonsNow}/>
-						</div>
+				<div className='pt-20'>
+					<div className='flex w-screen h-full items-center justify-center'>
+						<p className='text-black text-[200px]'>🙏</p>
+					</div>
+					<div className='flex w-screen h-full items-center justify-center'>
+						<p className='text-black text-4xl'>
+							Halaman Ini Masih Dalam Tahap Pengembangan
+						</p>
+					</div>
+					<div className='flex w-screen h-full items-center justify-center pt-4'>
+						<button className='text-black font-semibold cursor-pointer hover:text-yellow-600 underline text-xl' onClick={backPage}>Kembali</button>
 					</div>
 				</div>
 			</Layout>
@@ -26,4 +33,4 @@ const Season = async () => {
 	);
 };
 
-export default Season;
+export default Page;
