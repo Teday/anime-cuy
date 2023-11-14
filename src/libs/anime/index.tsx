7f4d296a56e2d5219e705633d2643d7384a7888a
Tedi Suryadi <tedi.suryadi77@gmail.com>
2023-11-14 19:09:23 +0700

    fix ++

--- src/libs/anime/index.tsx
+++ src/libs/anime/index.tsx
@@ -9,12 +9,7 @@ export const getAnime = async ( url: string ) => {
 export const getAnimeNested = async ( url: string, objectKey: string ) => {
 
 	const response = await fetch(`${process.env.base_url}/${url}`);
-	const anime: any = await response.json()
-	let datas: any = []
-    anime.data.map((item: any) => {
-		item.entry.map( (res:any) => {
-			datas.push(res)
-		})
-	})
-	return { data: datas }
+	const anime = await response.json()
+	const animeData = anime.data?.flatMap((item: any) => item[objectKey])
+	return { data: animeData }
 };
