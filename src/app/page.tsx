f1f07c5a9843dadb42e14c5531011ada22b9d102
Tedi Suryadi <tedi.suryadi77@gmail.com>
2023-11-15 00:05:47 +0700

    fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import { Layout, List } from "@/components";
 import { getAnime, getAnimeNested } from '@/libs';
+import { randomAnime } from "@/utils";
 
 const Page = async () => {
 
 	const animePopuler = await getAnime("top/anime?limit=15")
 	const animeLastUpdate = await getAnime("watch/episodes?limit=15")
 	const animeRecommendations = await getAnimeNested("recommendations/anime", "entry")
+	const animeRecom = randomAnime(animeRecommendations, 15)
 	
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between'>
 			<Layout>
 				<List anime={animePopuler} title="Top Anime" url="/anime/top" episode={false}/>
-				<List anime={animeRecommendations} title="Anime Rekomendasi" url="" episode={false}/>
+				<List anime={animeRecom} title="Anime Rekomendasi" url="" episode={false}/>
 				<List anime={animeLastUpdate} title="Last Update" url="" episode={true}/>
 			</Layout>
 		</main>
--- src/libs/anime/index.tsx
+++ src/libs/anime/index.tsx
@@ -1,5 +1,3 @@
-import { randomAnime } from "@/utils";
-
 export const getAnime = async ( url: string ) => {
 
 	const response = await fetch(`${process.env.base_url}/${url}`);
@@ -13,6 +11,5 @@ export const getAnimeNested = async ( url: string, objectKey: string ) => {
 	const response = await fetch(`${process.env.base_url}/${url}`);
 	const anime = await response.json()
 	const animeData = anime.data?.flatMap((item: any) => item[objectKey])
-	const datas = randomAnime(animeData, 15)
-	return datas
+	return animeData
 };
