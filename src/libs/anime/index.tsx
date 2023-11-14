a9dbde68de4f4b243bb7bb5bf97977c1102bd3e1
Tedi Suryadi <tedi.suryadi77@gmail.com>
2023-11-14 19:39:57 +0700

    adsd

--- src/libs/anime/index.tsx
+++ src/libs/anime/index.tsx
@@ -6,14 +6,6 @@ export const getAnime = async ( url: string ) => {
 
 };
 
-export const getAnime2 = async ( url: string ) => {
-
-	const response = await fetch(`${process.env.base_url}/${url}`);
-	const anime = await response.json()
-    return anime
-
-};
-
 export const getAnimeNested = async ( url: string, objectKey: string ) => {
 
 	const response = await fetch(`${process.env.base_url}/${url}`);
