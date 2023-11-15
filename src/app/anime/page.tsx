4396ca57369196ae14b3051c39bf20508a4e01cb
Tedi Suryadi <tedi.suryadi77@gmail.com>
2023-11-15 12:08:35 +0700

    fixing page anime

--- src/app/anime/page.tsx
+++ src/app/anime/page.tsx
@@ -1,21 +1,90 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Layout, List } from "@/components";
-import { getAnime, getAnimeNested } from '@/libs';
+import { getAnime, getAnimeNested } from "@/libs";
+import { randomAnime } from "@/utils";
 
-const AnimePage = async () => {
+const Page = () => {
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [animePopuler, setAnimePopuler] = useState<any>([]);
+	const [animeUpcoming, setAnimeUpcoming] = useState<any>([]);
+	const [animeLastUpdate, setAnimeLastUpdate] = useState<any>([]);
+	const [animeRecommendation, setAnimeRecommendation] = useState<any>([]);
+	const [animeSeason, setAnimeSeason] = useState<any>([]);
 
-	const animePopuler = await getAnime("top/anime?limit=15")
-	const animeLastUpdate = await getAnime("watch/episodes?limit=15")
-	const animeRecommendations = await getAnimeNested("recommendations/anime", "entry")
-	
+	useEffect(() => {
+		fetchData();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	const fetchData = async () => {
+		const getAnimeSeason = await getAnime("seasons/now?limit=15");
+		const getAnimeUpcoming = await getAnime("seasons/upcoming?limit=15");
+		const getAnimePopuler = await getAnime("top/anime?limit=15");
+		const getAnimeLastUpdate = await getAnime("watch/episodes?limit=15");
+		const getAnimeRecommendations = await getAnimeNested(
+			"recommendations/anime",
+			"entry"
+		);
+		const getAnimeRecom = randomAnime(getAnimeRecommendations, 15);
+
+		setAnimeSeason(getAnimeSeason);
+		setAnimeUpcoming(getAnimeUpcoming);
+		setAnimePopuler(getAnimePopuler);
+		setAnimeLastUpdate(getAnimeLastUpdate);
+		setAnimeRecommendation(getAnimeRecom);
+		setIsLoading(false);
+	};
+
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between'>
 			<Layout>
-				<List anime={animePopuler} title="Top Anime" url="/anime/top" episode={false}/>
-				<List anime={animeRecommendations} title="Anime Rekomendasi" url="" episode={false}/>
-				<List anime={animeLastUpdate} title="Last Update" url="" episode={true}/>
+				{isLoading ? (
+					<div className='flex w-screen h-screen items-center justify-center'>
+						<div className='custom-loader'></div>
+					</div>
+				) : (
+					<>
+						<List
+							anime={animeSeason}
+							title={
+								animeSeason.data === undefined
+									? "-"
+									: `${animeSeason.data[0].season} ${animeSeason.data[0].year}`
+							}
+							url='/anime/season'
+							episode={false}
+						/>
+						<List
+							anime={animePopuler}
+							title='Top Anime'
+							url='/anime/top'
+							episode={false}
+						/>
+						<List
+							anime={animeRecommendation}
+							title='Anime Rekomendasi'
+							url=''
+							episode={false}
+						/>
+						<List
+							anime={animeLastUpdate}
+							title='Last Update'
+							url=''
+							episode={true}
+						/>
+						<List
+							anime={animeUpcoming}
+							title='Anime Upcoming'
+							url=''
+							episode={false}
+						/>
+					</>
+				)}
 			</Layout>
 		</main>
 	);
 };
 
-export default AnimePage;
+export default Page;
